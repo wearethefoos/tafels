093488090310fdc99a4da5c65ad740a64118999d
Wouter de Vos <wouter@codaisseur.com>
2020-03-18 19:34:07 +0100

    Updates

---
+++ precache-manifest.54dcbc07cec7aedf94333ada3f211dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c09f454b37248afeedc912178483d8",
+    "url": "/tafels/index.html"
+  },
+  {
+    "revision": "aec6f2a265f5fd0a619f",
+    "url": "/tafels/static/css/main.44e7fec5.chunk.css"
+  },
+  {
+    "revision": "a368344f0282e8e6329a",
+    "url": "/tafels/static/js/2.dd30dec1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tafels/static/js/2.dd30dec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec6f2a265f5fd0a619f",
+    "url": "/tafels/static/js/main.f84b0239.chunk.js"
+  },
+  {
+    "revision": "6a7bf73f4b45cc7d0a3a",
+    "url": "/tafels/static/js/runtime-main.eccdd856.js"
+  }
+]);
